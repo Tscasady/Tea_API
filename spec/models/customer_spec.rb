f2be359cc3a8ebc7284746716416f5b6421d62e6
Thomas Casady <tscasady@gmail.com>
2023-04-19 16:06:14 -0500

    Add missing relation to teas through subscriptions for customer.

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Customer, type: :model do
   describe 'relations' do
     it { should have_many :subscriptions }
+    it { should have_many(:teas).through(:subscriptions) }
   end
 
   describe 'validations' do

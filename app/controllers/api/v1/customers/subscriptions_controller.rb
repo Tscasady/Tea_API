f45768a4cab70075c78e5648bdc157bd69b20942
Thomas Casady <tscasady@gmail.com>
2023-04-20 10:12:57 -0500

    fix: Solve an n+1 query.

--- app/controllers/api/v1/customers/subscriptions_controller.rb
+++ app/controllers/api/v1/customers/subscriptions_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::Customers::SubscriptionsController < ApplicationController
   def index
-    customer = Customer.find(params[:customer_id])
-    render json: SubscriptionSerializer.new(Subscription.where(customer: customer))
+    customer = Customer.includes(:subscriptions).find(params[:customer_id])
+    render json: SubscriptionSerializer.new(customer.subscriptions)
   end
 end

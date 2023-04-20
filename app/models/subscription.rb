959ea7ba3f48ee3ad1a9daca2b181705a8ae29d2
Thomas Casady <tscasady@gmail.com>
2023-04-20 12:07:01 -0500

    Add validations for numeric fields in subscription modle.

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -3,4 +3,7 @@ class Subscription < ApplicationRecord
   belongs_to :customer
 
   enum status: %i[active cancelled]
+
+  validates_numericality_of :frequency
+  validates_numericality_of :price
 end
--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -5,4 +5,9 @@ RSpec.describe Subscription, type: :model do
     it { should belong_to :tea }
     it { should belong_to :customer }
   end
+
+  describe 'validations' do
+    it { should validate_numericality_of :frequency }
+    it { should validate_numericality_of :price }
+  end
 end
